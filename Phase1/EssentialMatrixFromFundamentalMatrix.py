085f4cc813589b35a62b6bff9e55990163990436
sixer51 <sixerwyj@gmail.com>
2023-02-15 23:18:06 -0500

    estimate fundamental and essential matrix

--- Phase1/EssentialMatrixFromFundamentalMatrix.py
+++ Phase1/EssentialMatrixFromFundamentalMatrix.py
@@ -2,3 +2,18 @@
 Given fundamental matrix F
 Estimate essential matrix E
 '''
+
+import numpy as np
+
+def EssentialFromFundamental(F, K):
+    F = np.array(F)
+    K = np.array(K)
+
+    E = np.matmul(K.T, np.matmul(F, K))
+    U, S, VT = np.linalg.svd(E)
+    # print(S)
+    S = np.eye(1, dtype=float)
+    S[2, 2] = 0.
+    E = np.matmul(U, np.matmul(S, VT))
+
+    return E
--- Phase1/EstimateFundamentalMatrix.py
+++ Phase1/EstimateFundamentalMatrix.py
@@ -1,3 +1,30 @@
 '''
-estimate fundamental matrix F by solving linear least square
+Given N >= 8 point pairs
+Estimate fundamental matrix F by solving linear least square
 '''
+
+import numpy as np
+
+def EstimateFundamentalMatrix(x1s, x2s):
+    # x2 F x1 = 0
+    length = len(x1s)
+    A = []
+
+    for i in range(length):
+        x1 = x1s[0]
+        y1 = x1s[1]
+        x2 = x2s[0]
+        y2 = x2s[1]
+        A.append([x1 * x2, x1 * y2, x1,
+                  y1 * x2, y1 * y2, y1,
+                  x2, y2, 1])
+        
+    A = np.array(A)
+    _, _, VT = np.linalg.svd(A)
+    F = VT[-1, :].reshape((3, 3))
+
+    U, S, VT = np.linalg.svd(F)
+    S[2, 2] = 0.
+    F = np.matmul(U, np.matmul(S, VT))
+
+    return F
