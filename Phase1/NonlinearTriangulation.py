ca4af36ddff6303ff73ad17ad05260531d54433c
sixer51 <sixerwyj@gmail.com>
2023-02-24 21:42:18 -0500

    add nonlinear triangulation

--- Phase1/NonlinearTriangulation.py
+++ Phase1/NonlinearTriangulation.py
@@ -4,6 +4,7 @@ Refine 3D points by minimizing reprojection error
 '''
 
 import numpy as np
+from scipy.optimize import least_squares
 
 def reprojection(K, R, C, Xs):
     t = C.reshape((3,1))
@@ -16,6 +17,31 @@ def reprojection(K, R, C, Xs):
         x = P @ X
         u = x[0] / x[2]
         v = x[1] / x[2]
-        reprojImagePoints.append((u, v))
+        reprojImagePoints.append([u, v])
 
-    return reprojImagePoints
+    return np.array(reprojImagePoints)
+
+def flattenWorldPoint(Xs):
+    return Xs.reshape(-1)
+
+def recoverWorldPoints(x0):
+    return x0.reshape(-1, 3)
+
+def reprojError(x0, K, R1, C1, R2, C2, x1s, x2s):
+    reprojImagePoints1 = reprojection(K, R1, C1, recoverWorldPoints(x0))
+    reprojImagePointsFlatten1 = reprojImagePoints1.reshape(-1)
+    x1sError = x1s.reshape(-1) - reprojImagePointsFlatten1
+
+    reprojImagePoints2 = reprojection(K, R2, C2, recoverWorldPoints(x0))
+    reprojImagePointsFlatten2 = reprojImagePoints2.reshape(-1)
+    x2sError = x2s.reshape(-1) - reprojImagePointsFlatten2
+
+    error = np.hstack((x1sError, x2sError))
+    return error
+
+def nonlinearTriangulation(K, R1, C1, R2, C2, x1s, x2s, Xs):
+    x0 = flattenWorldPoint(Xs)
+    result = least_squares(reprojError, x0, args=(K, R1, C1, R2, C2, x1s, x2s), method='lm')
+    Xs = recoverWorldPoints(result.x)
+    return Xs
+
